45c2ac5af994c9e76432744e8433002b5504269e
RodolfoCarius <rcarius@gmail.com>
2023-08-18 16:20:44 -0300

    Resolução dos exercícios de list

---
+++ collectionJava/src/main/java/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,57 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+
+	private List<Integer>  numeros;
+	
+	
+	public OrdenacaoNumeros() {
+		this.numeros = new ArrayList<>();
+	}
+
+
+
+	public List<Integer> getNumeros() {
+		return numeros;
+	}
+	
+	public void setNumeros(List<Integer> numeros) {
+		this.numeros = numeros;
+	}
+	
+	
+	public void  adicionarNumero(int numero) {
+		numeros.add(numero);
+		return ;
+	}
+
+
+	public List<Integer> ordenarAscendente(){
+		List<Integer> numerosOrdemCrescente = new ArrayList<>(numeros);
+		Collections.sort(numerosOrdemCrescente);
+		return numerosOrdemCrescente;
+	}
+
+
+	public List<Integer> ordenarDescendente(){
+		List<Integer> numerosOrdemDecrescente = new ArrayList<>(numeros);
+		numerosOrdemDecrescente.sort(Collections.reverseOrder());
+		return numerosOrdemDecrescente;
+	}
+
+
+	public static void main(String[] args) {
+		OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
+
+		ordenacaoNumeros.adicionarNumero(10);
+		ordenacaoNumeros.adicionarNumero(5);
+		ordenacaoNumeros.adicionarNumero(7);
+
+		System.out.println(ordenacaoNumeros.ordenarAscendente());
+		System.out.println(ordenacaoNumeros.ordenarDescendente());
+	}
+}
